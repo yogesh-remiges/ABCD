325ce00c58706dd5ed5b3c2fe54c22fb4310f933
Vikas Tawade <vikas.tawade@remiges.tech>
2024-04-26 10:45:57 +0530

    TestByVicky

--- src/practice/hello.java
+++ src/practice/hello.java
@@ -10,6 +10,7 @@ public class hello {
 		System.out.println("tommorow!!!!");
 		System.out.println("3 Idiots!!!!");
 		System.out.println("Git practice by VIKAS");
+		System.out.println("Git practice by VIKAS on 26-April-2023");
 		
 	}
 
